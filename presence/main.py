c00547fab38a34407a376f0e302c82ae3c76c30b
jmechnich <joerg.mechnich@gmail.com>
2014-12-01 00:12:47 +0100

    added commandline option for killing existing process

--- presence/main.py
+++ presence/main.py
@@ -1,6 +1,6 @@
 from server import PresenceServer
 
-import argparse, lockfile, logging, os, sys
+import argparse, lockfile, logging, os, signal, sys, psutil, time
 
 from daemon import DaemonContext
 
@@ -33,6 +33,8 @@ def main(name, client_args={}):
     parser = argparse.ArgumentParser(description=name)
     parser.add_argument( '-d', '--daemon',  action="store_true",
                          help='run as daemon')
+    parser.add_argument( '-k', '--kill',  action="store_true",
+                         help='kill running instance if any before start')
     parser.add_argument( '-v', '--verbose', action="store_true",
                          help='')
     args = parser.parse_args()
@@ -48,13 +50,33 @@ def main(name, client_args={}):
         lock = os.path.join(os.environ['HOME'],'.%s' % name)
         
     if os.path.exists(lock+'.lock'):
-        print "Process already running (lockfile exists), exiting"
-        sys.exit(1)
-    else:
-        print lock, 'does not exist'
+        if args.kill:
+            pidfile = open(lock+'.lock','r')
+            pid = int(pidfile.readline().strip())
+            pidfile.close()
+            print "Sending SIGTERM to", pid
+            os.kill(pid,signal.SIGTERM)
+            retries = 5
+            while psutil.pid_exists(pid) and retries:
+                retries -= 1
+                time.sleep(1)
+            if psutil.pid_exists(pid):
+                print "Sending SIGKILL to", pid
+                os.kill(pid,signal.SIGKILL)
+                time.sleep(1)
+            if psutil.pid_exists(pid):
+                print "Could not kill", pid
+                print "Kill manually"
+                sys.exit(1)
+        else:
+            print "Process already running (lockfile exists), exiting"
+            sys.exit(1)
         
     if args.daemon:
         with DaemonContext(umask=0o002, pidfile=lockfile.FileLock(lock)):
+            pidfile = open(lock+'.lock', 'w')
+            print>>pidfile, os.getpid()
+            pidfile.close()
             _main(name, args.daemon, loglevel, client_args)
     else:
             _main(name, args.daemon, loglevel, client_args)
